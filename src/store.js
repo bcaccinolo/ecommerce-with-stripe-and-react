ad62300b97190951d7b798d808da4610b6ed6415
Benoit Caccinolo <bcaccinolo@synbioz.com>
2018-03-06 13:22:19 +0100

    using 'switch' in reducers

--- src/store.js
+++ src/store.js
@@ -2,30 +2,33 @@ import { combineReducers, createStore } from 'redux';
 
 // reducer to update the quantity
 function quantity(state = 1, action) {
-  if (action.type === 'INCREASE') {
+  switch (action.type) {
+  case 'INCREASE':
     return state + 1;
+  case 'DECREASE':
+    return Math.max(state - 1, 1);
+  default:
+    return state;
   }
-  if (action.type === 'DECREASE') {
-    let newState = state - 1;
-    if (newState < 1) { return 1; }
-    return newState;
-  }
-  return state;
 }
 
 // reducer to update unit price
 function unitPrice(state = 9.99, action) {
-  if (action.type === 'UPDATE_UNIT_PRICE') {
+  switch (action.type) {
+  case 'UPDATE_UNIT_PRICE':
     return action.price;
+  default:
+    return state;
   }
-  return state;
 }
 
 function clientName(state = 'John Doe', action) {
-  if (action.type === 'UPDATE_CLIENT_NAME') {
-    return action.name;
+  switch (action.type) {
+  case 'UPDATE_CLIENT_NAME':
+    return action.price;
+  default:
+    return state;
   }
-  return state;
 }
 
 // composition of the state with reducers
